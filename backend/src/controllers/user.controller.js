b9d24f005d57fbfc520ad0f1b64a09c5647e3ba2
San123san <147592258+San123san@users.noreply.github.com>
2024-11-15 21:49:54 +0530

    Update user.controller.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -124,7 +124,7 @@ const logoutUser = asyncHandler(async (req, res) => {
     const options = {
         httpOnly: true,
         secure: true,
-        sameSite: 'None'
+        // sameSite: 'None'
     }
 
     return res
