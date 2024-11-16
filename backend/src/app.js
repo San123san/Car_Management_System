f65cab0cb2db6484d6b22e36f72d7d65f3f414d2
San123san <147592258+San123san@users.noreply.github.com>
2024-11-16 07:48:41 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,22 +13,8 @@ app.use(cors({
 
 app.use(express.json({limit: "50mb"}))   //data take when fill form in the format of json
 app.use(express.urlencoded({extended: true, limit:"50mb"}))
-// app.use(express.static("public"))
-// Serve static assets from the public folder (images, documents, etc.)
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static("public"))
 app.use(cookieParser())
-
-// Serve static files from Vite build (dist folder)
-if (process.env.NODE_ENV === 'production') {
-    // Set the static folder to 'dist' (which is where your Vite build files are)
-    app.use(express.static(path.join(__dirname, 'dist')));
-  
-    // For any route that doesn't match an API route, serve the React index.html file
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-    });
-  }
 
 
 // routes import
