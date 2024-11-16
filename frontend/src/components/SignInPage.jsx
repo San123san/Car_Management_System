ce93d208b19db7a6ae1b66b29d001a075a34313f
San123san <s35102751@gmail.com>
2024-11-16 05:42:55 +0530

    tenth

--- frontend/src/components/SignInPage.jsx
+++ frontend/src/components/SignInPage.jsx
@@ -12,7 +12,8 @@ const SignInPage = () => {
 
     try {
       const response = await axios.post('https://car-management-system-fyne-assessment.onrender.com/api/v1/users/login', { email, password }, { withCredentials: true });
-
+      console.log(response)
+      console.log(response.data)
       localStorage.setItem('accessToken', response.data.data.accessToken);
       localStorage.setItem('refreshToken', response.data.data.refreshToken);
 
